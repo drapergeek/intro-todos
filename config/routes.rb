e761f4403bde6e101900d4844585b6bb363e6206
Jason Draper <jason@drapergeek.com>
2012-03-26 14:45:11 -0400

    added the todos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 IntroTodos::Application.routes.draw do
   resource :sessions, :only=>[:new, :create, :destroy]
-  resources :todos, :only=>[:index]
+  resources :todos, :only=>[:index, :new, :create]
   match "/sign_up", to: "sessions#new"
 
   root to: "high_voltage/pages#show", id: "homepage"
